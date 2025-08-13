837187e1455d15d4df6ceced41942f27f6dc8d0d
kimdomenick <kdomenick@thinkoomph.com>
2025-08-13 16:51:28 -0400

    Add boolean flag for archive

--- schemaTypes/article.ts
+++ schemaTypes/article.ts
@@ -47,5 +47,10 @@ export default defineType({
       type: 'array',
       of: [{type: 'block'}],
     }),
+    defineField({
+      name: 'archive',
+      title: 'Archive',
+      type: 'boolean',
+    }),
   ],
 })
